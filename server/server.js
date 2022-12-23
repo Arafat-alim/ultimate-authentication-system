1bb8dbace7a574409ef4a7ab19526393cff18b47
Arafat Alim <Arafat97alim@gmail.com>
2022-12-24 01:16:26 +0530

    Connected to Mongodb successfully

--- server/server.js
+++ server/server.js
@@ -1,8 +1,31 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const app = express();
 
-//! Imports
+//! Connect to DB
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB is connected Successfully"))
+  .catch((err) => console.log("DB is not Connected!", err));
+
+//! App middleware
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+// app.use(cors()); // allows all origins
+
+if ((process.env.NODE_ENV = "development")) {
+  app.use(cors({ origin: "http://localhost:3000" }));
+}
+//! Imports routes
 const authRoutes = require("./routes/authRoutes");
 
 //! Middleware
@@ -12,5 +35,5 @@ const PORT = process.env.port || 8000;
 
 //! Listing server
 app.listen(PORT, () => {
-  console.log(`Server is listening at ${PORT}`);
+  console.log(`Server is listening at ${PORT} - ${process.env.NODE_ENV}`);
 });
