9d02c3f90a5c354e6846c32667fd70fd7ab161ce
Arafat Alim <Arafat97alim@gmail.com>
2022-12-25 19:11:40 +0530

    Signup form

--- client/src/auth/Signup.jsx
+++ client/src/auth/Signup.jsx
@@ -6,10 +6,68 @@ import axios from "axios";
 import "react-toastify/dist/ReactToastify.css";
 
 const Signup = () => {
+  //! state
+  const [values, setValues] = useState({
+    name: "",
+    email: "",
+    password: "",
+    buttonText: "Submit",
+  });
+
+  //! Destructuring
+  const { name, email, password, buttonText } = values;
+
+  //! Handle change
+  const handleChange = (name) => (event) => {};
+
+  //! Click Submit
+  const clickSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  const signup = () => (
+    <form>
+      <div className="form-group">
+        <label className="text-muted">Name</label>
+        <input
+          type="text"
+          className="form-control"
+          onChange={handleChange("name")}
+          value={name}
+        />
+      </div>
+      <div className="form-group">
+        <label className="text-muted">Email ID</label>
+        <input
+          type="email"
+          className="form-control"
+          onChange={handleChange("email")}
+          value={email}
+        />
+      </div>
+      <div className="form-group">
+        <label className="text-muted">Password</label>
+        <input
+          type="password"
+          className="form-control"
+          onChange={handleChange("password")}
+          value={password}
+        />
+      </div>
+      <div className="form-group">
+        <button className="btn btn-primary" onClick={clickSubmit}>
+          {buttonText}
+        </button>
+      </div>
+    </form>
+  );
   return (
     <Layout>
-      <ToastContainer />
-      <h1>Signup</h1>
+      <div className="col-md-6 offset-md-3">
+        <ToastContainer />
+        <h1 className="p-5 text-center">Signup</h1>
+        {signup()}
+      </div>
     </Layout>
   );
 };
