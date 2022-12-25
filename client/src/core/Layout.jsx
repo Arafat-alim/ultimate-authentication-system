72b4ada1ffad00e42dede65364e6dd8e88b60254
Arafat Alim <Arafat97alim@gmail.com>
2022-12-26 03:06:15 +0530

    Activation Link Highlight

--- client/src/core/Layout.jsx
+++ client/src/core/Layout.jsx
@@ -1,21 +1,28 @@
-import { Link } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
 
-const Layout = ({ children }) => {
+const Layout = ({ children, match }) => {
+  const isActive = (path) => {
+    if (match.path === path) {
+      return { color: "#000", backgroundColor: "#fff" };
+    } else {
+      return { color: "#fff" };
+    }
+  };
   const nav = () => {
     return (
       <ul className="nav nav-tabs bg-primary">
         <li className="item">
-          <Link to="/" className="text-light nav-link">
+          <Link to="/" className="nav-link" style={isActive("/")}>
             Home
           </Link>
         </li>
         <li className="item">
-          <Link to="/signup" className="text-light nav-link">
+          <Link to="/signup" className=" nav-link" style={isActive("/signup")}>
             Signup
           </Link>
         </li>
         <li className="item">
-          <Link to="/login" className="text-light nav-link">
+          <Link to="/login" className="nav-link" style={isActive("/login")}>
             Login
           </Link>
         </li>
@@ -30,4 +37,4 @@ const Layout = ({ children }) => {
   );
 };
 
-export default Layout;
+export default withRouter(Layout);
